3a688d1bb542998e7b04e919e218e3eaf932510d
Shane Henry <37082848+nitrodrip@users.noreply.github.com>
2018-07-08 17:12:05 -0600

    Add files via upload

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+	
 var config = {
     apiKey: "AIzaSyBgCooaMRU1_c_pR0Quoy8o3x-KytrMVew",
     authDomain: "trainschedule-be902.firebaseapp.com",
@@ -6,49 +8,47 @@ var config = {
     storageBucket: "trainschedule-be902.appspot.com",
     messagingSenderId: "295353835580"
   };
-  
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
  
-var trainData = firebase.database();
+var datebase = firebase.database();
 var apiKey = "ip1soj992i";
 var arrivalUrl = "https://api.railwayapi.com/v2/arrivals/station/cdg/hours/2/apikey/ip1soj992i/";
-console.log (trainData);
+console.log (datebase);
 
+	// On Click for sumbmit button with prevent to stop reloading
+	$("#add-train-btn").on('click', function(event){
+	event.preventDefault();
+	//console.log("working");
 
-  // Button for adding trains
+  	// On click event Button for adding trains
 	$("#add-train-btn").on("click", function() 
+						   
+	// Grabs user input from forms
+  	var numberInput = $("#number-input").val().trim();
+	var trainName = $("#train-name-input").val().trim();
 
-  // Grabs user input
-  var trainName = $("#train-name-input").val().trim();
-  var numberInput = $("#number-input").val().trim();
-
- // Creates local "temporary" object for holding train data
-  var newTrain = {
+ 	// Creates local "temporary" object for holding train data
+  	var newTrain = {
     name: trainName,
 	schdep: scheduledDepart,
 	actdep: actualDepart,
   };
 
-	//Load JSON values
+	// Load JSON values
 	function trainRoutes {
 	loadJSON (url, gotData);
 	console.log (data);
 	}
 
 
-  // Upload train data to the database
-  trainData.ref().push(newTrain);
+  	// Upload train data to the database
+  	database.ref().push(newTrain);
 
-  // Logs everything to console
-  console.log(newTrain.name);
-  console.log(newTrain.scheduledDepart);
-  console.log(newTrain.actualDepart);
+  	// Alert
+  	alert("Train successfully added");
 
-  // Alert
-  alert("Train successfully added");
-
-  // Clears all of the text-boxes
-  $("#train-name-input").val("");
-  $("#number-input").val("");
+	  // Clears all of the text-boxes
+	  $("#train-name-input").val("");
+	  $("#number-input").val("");
 
 
